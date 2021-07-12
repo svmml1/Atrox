31c8b700c5f2beab1252ee66f1714f16b348fd53
Sergio Victor <sergiovmml@gmail.com>
2021-07-12 19:48:27 -0300

    11

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import background from './images/lolbackground.jpg'
 import  {Header}  from './components/Header'
 import './App.css';
