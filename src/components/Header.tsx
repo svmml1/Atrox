dd466ecbbba5192562889816ff267832fc23972f
Sergio Victor <sergiovmml@gmail.com>
2021-07-12 13:54:02 -0300

    1

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface HeaderProps {
+    items: string[];
+}
+
+export function Header({items }: HeaderProps){
+    return (
+        <div className="header">
+
+        </div>
+    )
+}
