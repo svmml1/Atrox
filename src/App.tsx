0bf3e6dcfcfbd4d687df62e9e159545eb452bf01
Sergio Victor <sergiovmml@gmail.com>
2021-07-12 19:49:32 -0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import background from './images/lolbackground.jpg'
-import  {Header}  from './components/Header'
+import { Header } from './components/Header'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-        <Header  items={['Characters']}/>
-        <img src={background} className="background-image" alt="background" />
-        
-    </div>
+    <Router>
+      <main>
+        <div className="App">
+          <Header items={['Characters']} />
+          <img src={background} className="background-image" alt="background" />
+
+        </div>
+        <Route exact path="/" component={Home} />
+      </main>
+    </Router>
+
   );
 }
 
